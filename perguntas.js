cbaacba18bff99202ee5d05baa32315b0bd65e69
3A24Miguel <00001107054643sp@al.educacao.sp.gov.br>
2024-11-28 00:00:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Matematica',
+    'Qual é a valor de x nessa equação 2x=360÷4',
+    '45'
+)
+
+criaCartao(
+    'Historia',
+    'Quem assinou a lei áurea?',
+    'Princesa Isabel'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual fungo mais perigoso do mundo?',
+    'Aspergillus fumigatus'
+)
+
+criaCartao(
+    'Quimica',
+    'O Que é radiação?',
+    'ondas eletromagnéticas ou partículas que se propagam com uma determinada velocidade'
+)
+
+criaCartao(
+    'Educação Fisica',
+    'Qual esporte mais perigoso do mundo?',
+    'Boxe'
+)
+
+criaCartao(
+    'Inglês',
+    'Who is your favorite singer?',
+    'Taylor Swift'
+)
